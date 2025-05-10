5f48378d28bcacea12bfc5c506525158605e31c1
Kane Kabena <onekaneldn@gmail.com>
2025-05-10 15:08:14 +0300

    instance methods lab complete

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,17 @@
 
 class Person:
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
-    pass
+
+kane = Person()
+kane.talk()
+kane.walk()
+
+Fred = Person()
+Fred.talk()
+Fred.walk()
